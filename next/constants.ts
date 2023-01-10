61b09e630f698def2c6b93a9d3694046046c264a
Jon Lewis <jonathandavidlewis92@gmail.com>
2023-01-10 11:13:23 -0700

    refactor methods

--- next/constants.ts
+++ next/constants.ts
@@ -4,3 +4,7 @@ export const NFT_CONTRACT_NAME =
   process.env.NEXT_PUBLIC_NFT_CONTRACT_NAME || "keypom-beta-nfts.testnet";
 export const NETWORK =
   process.env.NEXT_PUBLIC_NEAR_NETWORK === "mainnet" ? "mainnet" : "testnet";
+
+export const WALLET_URL = `https://wallet.${
+  NETWORK === "testnet" ? `testnet.` : ``
+}near.org`;
---
+++ next/links.ts
@@ -0,0 +1,30 @@
+import { KeyPairEd25519 } from "near-api-js/lib/utils";
+import { CONTRACT_NAME, WALLET_URL } from "./constants";
+
+export const generateKeys = (num: number) => {
+  const keys = [];
+  for (let i = 0; i < num; i++) {
+    keys.push(KeyPairEd25519.fromRandom());
+  }
+  return keys;
+};
+
+const generateCsv = (fileName: string, data: string) => {
+  const file = new File([data], fileName);
+  const link = document.createElement("a");
+  link.setAttribute("visibility", "hidden");
+  // link.style.display = 'none';
+  link.href = URL.createObjectURL(file);
+  link.download = file.name;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+export const downloadLinks = async (dropId: string, keys: KeyPairEd25519[]) => {
+  // TODO this is hardcoded to use a specific wallet, this could be dynamic.
+  const links = keys.map(
+    ({ secretKey }) => `${WALLET_URL}/linkdrop/${CONTRACT_NAME}/${secretKey}`
+  );
+  generateCsv(`nft_drop_id_${dropId}_links.csv`, links.join("\r\n"));
+};
