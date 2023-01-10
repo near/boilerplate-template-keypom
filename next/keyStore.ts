637c8d24cf7055a7a210c5885e2289bf1fe05976
Jon Lewis <jonathandavidlewis92@gmail.com>
2023-01-10 11:13:41 -0700

    refactor methods

---
+++ next/keyStore.ts
@@ -0,0 +1,13 @@
+// Gets the secret keys for a drop.
+export const getKeysForDrop = (dropId: number | string): string[] =>
+  JSON.parse(window.localStorage.getItem(`drop#${dropId}`) || "[]");
+// Stores the secret keys for a drop.
+export const setKeysForDrop = (dropId: number | string, keys: string[]) =>
+  window.localStorage.setItem(`drop#${dropId}`, JSON.stringify(keys));
+
+export const setInactiveDrop = (dropId: number | string) => {
+  window.localStorage.setItem("inactiveDropId", dropId.toString());
+};
+
+export const getInactiveDrop = () =>
+  window.localStorage.getItem("inactiveDropId");
