a3f183d22c9dc0d6f4479e452177757c15d2abb4
Jon Lewis <jonathandavidlewis92@gmail.com>
2023-01-18 08:58:17 -0700

    add env scripts

--- contracts/dev-deploy.sh
+++ contracts/dev-deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-cd nft-series && near dev-deploy ./nft-series.wasm && cd ..
+cd nft-series
+
+near dev-deploy ./nft-series.wasm
+
+CONTRACT_ADDRESS=`cat ./neardev/dev-account`
+
+cd ../../frontend
+
+touch .env.local
+
+echo NEXT_PUBLIC_NFT_CONTRACT_NAME=$CONTRACT_ADDRESS >> .env.local
