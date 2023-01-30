b18094a8d96150200f931d6099adedb1882a695a
Dmitriy <sheleg.dmitriy@gmail.com>
2023-01-30 16:49:37 +0200

    Add loader and error icon to image

--- frontend/components/ViewDrops.tsx
+++ frontend/components/ViewDrops.tsx
@@ -60,6 +60,8 @@ function DropCard({ drop }: DropCardProps) {
   const router = useRouter();
   const [isDeleting, setIsDeleting] = useState(false);
   const [isPixelArt, setIsPixelArt] = useState(false);
+  const [imageLoading, setImageLoading] = useState(true);
+  const [isImageError, setImageError] = useState(false);
   const dropId = drop.drop_id;
   const keys = getKeysForDrop(dropId);
   const dropMetadata = JSON.parse(drop.metadata);
@@ -71,9 +73,14 @@ function DropCard({ drop }: DropCardProps) {
   useEffect(() => {
     const img = new Image();
     img.onload = () => {
+      setImageLoading(false);
       setIsPixelArt(img.naturalHeight < 256 && img.naturalWidth < 256);
     };
-    img.onerror = (err) => console.log('Failed to get image data', err);
+    img.onerror = (err) => {
+      setImageLoading(false);
+      setImageError(true);
+      return console.log('Failed to get image data', err)
+    };
     img.src = media;
   }, [media]);
 
@@ -90,12 +97,55 @@ function DropCard({ drop }: DropCardProps) {
 
   return (
     <div className="mx-auto mb-8 max-w-sm rounded overflow-hidden bg-gray-200 grayscale hover:grayscale-0">
-      <img
-        className="p-4 w-full aspect-square"
-        style={isPixelArt ? { imageRendering: 'pixelated' } : {}}
-        src={media}
-        alt="NFT Image"
-      />
+      <div
+        className="image-wrapper p-4 w-full aspect-square flex items-center justify-center relative"
+        title={isImageError ? "Error on loading image" : ""}
+      >
+        {imageLoading ? (
+          <svg
+            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white absolute"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth={4}
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        ) : null}
+        {isImageError ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 absolute"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+            />
+          </svg>
+        ) : null}
+        <img
+          className="p-4 w-full aspect-square"
+          style={isPixelArt ? { imageRendering: 'pixelated' } : {}}
+          src={media}
+          alt="NFT Image"
+        />
+      </div>
       <div className="p-4">
         <div className="px-4 py-4">
           <div className="font-bold text-xl mb-2 text-gray-500">#{dropId}</div>
